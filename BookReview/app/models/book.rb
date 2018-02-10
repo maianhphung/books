14a651a70fbe3b25bd1e1b5b1d3db9a25e8f94e3
mphung000@gmail.com <mphung000@gmail.com>
2018-02-09 22:09:01 -0500

    first commit

---
+++ BookReview/app/models/book.rb
@@ -0,0 +1,8 @@
+class Book < ApplicationRecord
+	belongs_to :user
+	belongs_to :category
+	belongs_to :review
+
+  has_attached_file :book_img, styles: { book_index: "250x350>", thumb: "325x475>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
+end
