a7ab60a5d16ad6a03b200f1e4657601d58373d65
mphung000@gmail.com <mphung000@gmail.com>
2018-02-15 23:27:27 -0500

    delete problem

--- BookReview/app/models/book.rb
+++ BookReview/app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
-	has_one :review
+	has_one :review , :dependent => :destroy
 
   has_attached_file :book_img, styles: { book_index: "250x350>", thumb: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
